67053d75095a2971265a07b35dfd817823197cac
Dong_D <ehdgud6634@gmail.com>
2023-03-04 13:27:37 +0900

    1094 막대기

--- baekjoon/1094/README.md
+++ baekjoon/1094/README.md
@@ -4,3 +4,5 @@
 [https://www.acmicpc.net/problem/1094](https://www.acmicpc.net/problem/1094)
 
 ## 해결방법
+- 주어진 조건과 상황에 맞게 반복+조건문 설정
+- 짤라낼 때는 cnt++ 하지 않고, 붙일때는 cnt++
--- baekjoon/1094/main.c
+++ baekjoon/1094/main.c
@@ -3,47 +3,81 @@
 #include <ctype.h>
 #include <unistd.h>
 
-int half_sum(int curr_mak, int X, int* cnt) {
-	int nam_mak = 0;
-
-	*cnt += 1;
-	if (X < curr_mak) {
-		curr_mak /= 2;
-		nam_mak = curr_mak;
-	}
-	if (X < nam_mak)
-		nam_mak += half_sum(nam_mak, X, cnt);
-	return (nam_mak);
-}
-
+/* X = answer */ 
 int main() {
-	int X = 0, cnt = 0, curr_mak = 64, nam_mak;
+	int X = 0, sum_mak = 64, half_mak = 0, cnt = 1;
 	scanf("%d", &X);
-	while (X != curr_mak) {
-		curr_mak = half_sum(curr_mak, X, &cnt);
+	half_mak = sum_mak / 2;
+	if (X != sum_mak) {
+		sum_mak -= half_mak;
+		while (X != sum_mak) {
+			if (X < sum_mak) {
+				half_mak /= 2;
+				sum_mak -= half_mak;
+			}
+			else if (X > sum_mak) {
+				sum_mak += half_mak / 2;
+				half_mak /= 2;
+				cnt++;
+			}
+		}
 	}
 	printf("%d\n", cnt);
 }
 
+/* debug */ 
+// int main() {
+// 	int X = 0, sum_mak = 64, half_mak = 0, cnt = 1;
+// 	scanf("%d", &X);
+// 	printf("INIT : sum %d, half %d, cnt %d\n", sum_mak, half_mak, cnt);
+// 	half_mak = sum_mak / 2;
+// 	if (X != sum_mak) {
+// 		sum_mak -= half_mak;
+// 		printf("[1] sum %d, half %d, cnt %d\n", sum_mak, half_mak, cnt);
+// 		while (X != sum_mak) {
+// 			if (X < sum_mak) {
+// 				half_mak /= 2;
+// 				sum_mak -= half_mak;
+// 				printf("X < sum_mak");
+// 			}
+// 			else if (X > sum_mak) {
+// 				sum_mak += half_mak / 2;
+// 				half_mak /= 2;
+// 				cnt++;
+// 				printf("X > sum_mak");
+// 			}
+// 			printf("sum %d, half %d, cnt %d\n", sum_mak, half_mak, cnt);
+// 			sleep(1);
+// 		}
+// 	}
+// 	printf("%d\n", cnt);
+// }
 
-int main() {
-	int X = 0, cnt = 0, curr_mak = 64, nam_mak = 0, sum = 0;
-	scanf("%d", &X);
-	while (X < curr_mak)
-		curr_mak /= 2;
-	sum += curr_mak;
-	if (sum == X) {
-		cnt = 1;
-	}
-	else {
-		while (sum - X < curr_mak)
-			curr_mak /= 2;
-	}
+/* X = 1~64 debug */ 
+// int main() {
+// 	int X = 0, sum_mak = 64, half_mak = 0, cnt = 1;
+// 	// scanf("%d", &X);
+// 	for (X = 1; X < 65; X++) {
+// 	sum_mak = 64;
+// 	cnt = 1;
+// 	half_mak = sum_mak / 2;
+// 	if (X != sum_mak) {
+// 		sum_mak -= half_mak;
+// 		while (X != sum_mak) {
+// 			if (X < sum_mak) {
+// 				half_mak /= 2;
+// 				sum_mak -= half_mak;
+// 			}
+// 			else if (X > sum_mak) {
+// 				sum_mak += half_mak / 2;
+// 				half_mak /= 2;
+// 				cnt++;
+// 			}
+// 			printf("[X %d], sum %d, half %d, cnt %d\n", X, sum_mak, half_mak, cnt);
+// 			// sleep(1);
+// 		}
+// 	}
+// 	printf("VAL : %d\n", cnt);
+// 	}
+// }
 
-
-	while (X != curr_mak) {
-		if
-		curr_mak = half_sum(curr_mak, X, &cnt);
-	}
-	printf("%d\n", cnt);
-}
